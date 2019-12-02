61237862922a0889ea75265977ebe1ea4bae7ed8
Tomas Gear <tomas.e.gear@gmail.com>
2019-12-02 13:26:50 -0500

    test123

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,6 +17,7 @@ module.exports = function(app) {
 
   // Delete an example by id
   // Hello
+  // line 2
   app.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
