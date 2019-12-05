07ac6a46f5a628777e4f09312bab55c29208d1b3
SP <sampizzo@gmail.com>
2019-12-05 11:49:22 -0500

    changed test password to process.env.MYSQL_PASSWORD

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   test: {
     username: "root",
-    password: null,
+    password: process.env.MYSQL_PASSWORD,
     database: "testdb",
     host: "localhost",
     dialect: "mysql",
