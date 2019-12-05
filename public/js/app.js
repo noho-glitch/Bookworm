8f84df3ad3389f068c3f5e815f7353bead30a2dd
Dave Nolan <dpn.nolan@gmail.com>
2019-12-05 14:00:32 -0500

    added css

--- public/js/app.js
+++ public/js/app.js
@@ -42,12 +42,11 @@ function renderBooks() {
     var author = response.items[0].volumeInfo.authors[0];
     var image = response.items[0].volumeInfo.imageLinks.thumbnail;
     var description = response.items[0].volumeInfo.description;
-    var rating = 
     
-    console.log(title)
-    console.log(author)
-    console.log(image)
-    console.log(description)
+    // console.log(title)
+    // console.log(author)
+    // console.log(image)
+    // console.log(description)
 
     var results = response.items;
 
@@ -63,29 +62,54 @@ function renderBooks() {
         rating = booksArr[i].volumeInfo.averageRating;
         image = booksArr[i].volumeInfo.imageLinks.thumbnail;
         description = booksArr[i].volumeInfo.description;
-
+        isbn = booksArr[i].volumeInfo.industryIdentifiers[0].identifier;
+        pageCount = booksArr[i].volumeInfo.pageCount;
+        publishedDate = booksArr[i].volumeInfo.publishedDate;
+       
 
         var card = $("<div>");
         card.addClass("card form-rounded");
 
+        var imageDiv = $("<div>");
+        imageDiv.addClass("float-left thumbnail")
+
+        var textDiv = $("<div>");
+        textDiv.addClass("float-left")
+        
+        var bookShelf = $("<button>");
+        bookShelf.addClass("btn btn-warning form-rounded");
+        bookShelf.text("Add to Bookshelf")
+
         var cardBody = $("<div>");
         cardBody.addClass("card-body");
 
-        var bookTitle = $("<p>");
+        var bookTitle = $("<h5>");
         bookTitle.text(title);
         bookTitle.addClass("card-text");
 
-       
+        var bookPageCount = $("<p>");
+        bookPageCount.addClass("card-text");
+        bookPageCount.text("Page Count: " + pageCount);
 
-        
+        var bookPublishedDate = $("<p>");
+        bookPublishedDate.addClass("card-text");
+        bookPublishedDate.text("Published Date: " + publishedDate);
+
+
+        var bookISBN = $("<p>");
+        bookISBN.addClass("card-text");
+        bookISBN.text("ISBN_10: " + isbn);
+       
+        var bookPageCount = $("<p>");
+        bookPageCount.addClass("card-text");
+        bookPageCount.text("Page Count: " + pageCount);
+
         var bookRating = $("<span>");
         bookRating.addClass("card-text rating data-default-rating=" + rating + " disabled")
-        // bookRating.text(rating);
-        // span.append(bookRating);
         console.log(rating)
 
         var bookAuthor = $("<p>");
-        bookAuthor = $("<p>");
+        bookAuthor.text("By: " + author);
         bookAuthor.addClass("card-text");
 
         var cardImage = $("<img>");
@@ -95,10 +119,20 @@ function renderBooks() {
        
        
 
-        cardBody.append(cardImage);
-        cardBody.append(bookTitle);
-        cardBody.append(bookRating);
-        cardBody.append(bookAuthor);
+        // cardBody.append(cardImage);
+        // cardBody.append(bookTitle);
+        // cardBody.append(bookAuthor);
+        // cardBody.append(bookRating);
+        // cardBody.append(bookPublishedDate);
+        // cardBody.append(bookISBN);
+        imageDiv.append(cardImage)
+        textDiv.append(bookTitle);
+        textDiv.append(bookAuthor);
+        // textDiv.append(bookPublishedDate);
+        // textDiv.append(bookISBN);
+        textDiv.append(bookShelf);
+        cardBody.append(imageDiv);
+        cardBody.append(textDiv);
         card.append(cardBody)
         $("#resultsDiv").append(card)
     }
