db04397ff1c00e60c17ea1b8ea66dcde62d172be
SP <sampizzo@gmail.com>
2019-12-08 16:43:10 -0500

    Updated authcontroller.js with logout and redirect

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -20,6 +20,7 @@ exports.search = function (req, res) {
 
 exports.logout = function(req, res) {
     req.session.destroy(function(err) {
-        res.redirect('/');
+        console.log("User logged out");
     });
+    res.redirect('/');    
 }
