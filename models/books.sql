59d8bea6ca35fa5d9093b7ff399752de53d7e82a
Tomas Gear <tomas.e.gear@gmail.com>
2019-12-09 14:41:20 -0500

    test

--- models/books.sql
+++ models/books.sql
@@ -5,4 +5,4 @@ ALTER TABLE books CHANGE COLUMN updatedAt updatedAt datetime NOT NULL DEFAULT CU
 
 ALTER TABLE books MODIFY ISBN BIGINT;
 
-INSERT INTO books (ISBN, title, authors, pageCount, userId) VALUES (40, "Bird Box", "Somebody", 300, 1);
+INSERT INTO books (ISBN, title, authors, pageCount, userId) VALUES (40, "Bird Box", "Somebody", 300, 1);    
