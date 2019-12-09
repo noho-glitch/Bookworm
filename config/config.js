ce4372c7c4f2e16cbcd20f0a5f37a138d358454d
Tomas Gear <tomas.e.gear@gmail.com>
2019-12-09 07:41:54 -0500

    heroku sync up with mysql

--- config/config.js
+++ config/config.js
@@ -16,7 +16,11 @@ module.exports = {
     logging: false
   },
   production: {
+    username: "y17gd7lfcn3qee1y",
+    password: "px0g09hlw813b0b4",
+    database: "kcif97jxfngsogln",
+    host: "hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mysql",
     use_env_variable: "JAWSDB_URL",
-    dialect: "mysql"
   }
 };
